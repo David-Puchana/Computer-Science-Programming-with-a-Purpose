5f24431530df44b992e3ab4479a9a3560a4b6d9e
David-Puchana <David.puchan@outlook.com>
2024-01-25 00:07:47 -0500

    Exercises 1 and 2

--- Code/CodeWeek_3/src/codeweek_3/CodeWeek_3.java
+++ Code/CodeWeek_3/src/codeweek_3/CodeWeek_3.java
@@ -9,13 +9,35 @@ package codeweek_3;
  * @author david
  */
 public class CodeWeek_3 {
+    public static String sumOnesBinary(int num) {        
+        if(num == 0){
+            return "+";
+        } else if (num == 1){
+            return "-";
+        } else {
+            int result = 0;                        
+            while(num >= 1 ){
+                if(num % 2 != 0){                
+                  result++;  
+                } 
+                num = num / 2;
+            }             
+            return (result % 2 == 0) ? "+":"-";                    
+        }        
+    }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-               
+
+        int m = 8;    
+        int count = 0;
+                                                 
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < m; j++){
+                if(j < m - 1) System.out.print(sumOnesBinary(count) + "  ");                
+                else System.out.print(sumOnesBinary(count));                 
+                count++;
+            }
+            System.out.println("");
+        }        
     }    
 }
